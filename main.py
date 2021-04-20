12d685e953f89ce2d40545b40c0c6ae42d9ec25a
Summer Shareh <samersamer1117@gmail.com>
2021-04-20 16:22:51 +0300

    'fix'

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import re
 
 
 def CheckString(Text):
+    number = 0
     m = re.search(r'[0-9]{4}', Text)
-    if len(m.group()) == 4 :
+    if m is not None:
         print("there is 4 Sequence digits in the string")
     result = re.sub(r"[a-zA-Z]", "*", Text)
     print(result)
+
+    for i in Text:
+        if i.isdigit():
+            number += 1
+            print(i)
+    if number >= 4:
+        print("there is 4 Sequence digits in the string")
+
 
 
 if __name__ == '__main__':
